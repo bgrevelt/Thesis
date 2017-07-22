159065f9144028fe87b0bfb021f8b43ba51c7147
Bouke Grevelt <bgr@qps.nl>
2017-07-22 12:26:22 +0200

    Added support for setting the compression mode for LZMA

--- testbench/algorithms/wclzma.py
+++ testbench/algorithms/wclzma.py
@@ -2,18 +2,27 @@ import os
 import pylzma
 import gwf
 import struct
+import json
+import logging
 
 _record_header_format = '<II' # block size including header, record ID
 header_length = struct.calcsize(_record_header_format)
 
+compression_mode = 2
+
 def init(parameters):
-    pass
+    decoded = json.loads(parameters)
+    global compression_mode
+    if 'compression mode' in decoded:
+        compression_mode =  decoded['compression mode']
+    else:
+        logging.warning('Parameters did not contain expected field "compression mode". using default compression mode {}'.format(compression_mode))
 
 def compress(input_path, output_path):
     gwf_file = gwf.File(input_path)
     with open(output_path, 'wb') as output_file:
         for record in gwf_file.read():
-            compressed = pylzma.compress(record.serialize())
+            compressed = pylzma.compress(record.serialize(), algorithm=compression_mode)
             write_block(compressed, record.ping_number, output_file)
 
 def decompress(input_path, output_path, record_id = None):
@@ -50,60 +59,3 @@ def write_block_header(data, record_id, file):
 def skip_block(file, block_size):
     compressed_data_size = block_size - header_length
     file.seek(compressed_data_size, 1)
-
-# def _compress(data):
-#     compressed_data_length = ctypes.c_uint()
-#     compressed = _compress_function(ctypes.c_char_p(data), ctypes.c_uint(len(data)), ctypes.byref(compressed_data_length))
-#     data = compressed[:compressed_data_length.value]
-#
-#     # We now have the data in python space, so the library can deallocate the data
-#     _destroy_function(compressed)
-#
-#     return data
-#
-# def _decompress(data):
-#     decompressed_data_length = ctypes.c_uint()
-#     decompressed = _decompress_function(ctypes.c_char_p(data), ctypes.c_uint(len(data)), ctypes.byref(decompressed_data_length))
-#     data = decompressed[:decompressed_data_length.value]
-#
-#     # We now have the data in python space, so the library can deallocate the data
-#     _destroy_function(decompressed)
-#
-#     return data
-#
-#
-# def decompress(input_path, output_path, record_id = None):
-#     if record_id == None:
-#         return full_decompress(input_path, output_path)
-#     else:
-#         return partial_decompress(input_path, output_path, record_id)
-#
-# def full_decompress(input_path, output_path):
-#     with open(input_path, 'rb') as in_file, open(output_path, 'wb') as out_file:
-#        obj = pylzma.decompressobj()
-#        while True:
-#            tmp = in_file.read(1)
-#            if not tmp:
-#                break
-#            decompressed = obj.decompress(tmp)
-#            if decompressed:
-#                out_file.write(decompressed)
-#
-#        decompressed = obj.flush()
-#        out_file.write(decompressed)
-#
-# def partial_decompress(input_path, output_path, record_id):
-#     # decompress the file to a temporary location
-#     temp_file = output_path + '_TEMP'
-#     full_decompress(input_path, temp_file)
-#
-#     #use GWF to read the entire file
-#     gwf_file = gwf.File(temp_file)
-#     for wc in gwf_file.read():
-#         if wc.ping_number == record_id:
-#             with open(output_path, 'wb') as f:
-#                 f.write(wc.serialize())
-#             break
-#
-#     gwf_file = None
-#     os.remove(temp_file)
