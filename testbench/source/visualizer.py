aefbf312ee2a8662511a26b3f991ad4fc5f44622
Bouke Grevelt <bgr@qps.nl>
2017-07-29 13:54:03 +0200

    Cleaned up visualizer and make sure that all graphs have the same file order.

--- testbench/source/visualizer.py
+++ testbench/source/visualizer.py
@@ -21,21 +21,11 @@ def visualize(result):
                 restructured_results[metric][(os.path.split(file)[1],algorithm)] = value
 
     result = restructured_results
-
-    #del result['Real-time compression time']
-    #del result['Random access decompression time']
     del result['Losslessness']
 
-    number_of_plots = len(result)
-    # width = math.ceil(math.sqrt(number_of_plots))
-    # height = math.ceil(number_of_plots / width)
-    # fig, xs = plt.subplots(nrows=height, ncols=width)
-    # axes = [a for list_of_axes in xs for a in list_of_axes]
-
-    print(result)
     for index, (metric, metric_results) in enumerate(result.items()):
         files, algorithms = zip(*metric_results.keys())
-        files = list(set(files))
+        files = sorted(list(set(files)))
         algorithms = sorted(list(set(algorithms)))
 
         number_of_bar_groups = len(files)
@@ -44,16 +34,12 @@ def visualize(result):
         width = 1 / (number_of_bars_per_group + 1)
         pos = [[i + width * algorithm_index for i in range(number_of_bar_groups)] for algorithm_index in range(len(algorithms))]
 
-        # Plotting the bars
-
-
         values = [[metric_results[(file, algorithms[algorithm_index])] for file in files] for algorithm_index in range(len(algorithms))]
 
-        fig, ax = plt.subplots()
+        fig, ax = plt.subplots(figsize = (20,5))
 
         hsv = hsv = mpl.cm.get_cmap('Dark2')
         colors = hsv(np.linspace(0.1, 0.9, len(algorithms)))
-
 
         for i in range(len(algorithms)):
             # Create a bar with pre_score data,
@@ -64,7 +50,7 @@ def visualize(result):
                     # of width
                     width,
                     # with alpha 0.5
-                    alpha=0.4,
+                    alpha=0.7,
                     # with color
                     color=colors[i],
                     # with label the first value in first_name
@@ -72,6 +58,9 @@ def visualize(result):
 
             for position,value in zip(pos[i], values[i]):
                 ax.text(position - width/2, value, '{:.3g}'.format(value), color=colors[i])
+
+        if metric == "Cost":
+            metric = "Cost reduction"
 
         # Set the y axis label
         ax.set_ylabel(metric)
@@ -85,23 +74,9 @@ def visualize(result):
         ticks = pos[len(pos) // 2]
         ax.set_xticks(ticks)
 
-        xticks = ['{}...{}'.format(file.split('_')[0], file.split('_')[-1]) for file in files]
+        xticks = ['{}...{}'.format(file.split('_')[0], file.split('_')[-1]) if len(file) > 15 else file for file in files]
         # Set the labels for the x ticks
         ax.set_xticklabels(xticks)
-
-        # use short labels for the files otherwise the graphs becomes unreadable / ugly
-        # xticks = ['File {}'.format(i+1) for i in range(len(files))]
-
-        # Setting the x-axis and y-axis limits
-        #plt.xlim(min(pos)-width, max(pos)+width*4)
-        #plt.ylim([0, max(df['pre_score'] + df['mid_score'] + df['post_score'])] )
-
-        # Then add a box showing the relation between the label and the actual file
-        # t = ['{} = {}'.format(xticks[i], files[i]) for i in range(len(files))]
-        # props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
-        # # place a text box in upper left in axes coords
-        # ax.text(0.05, 0.95, '\n'.join(t), transform=ax.transAxes, fontsize=14,
-        #         verticalalignment='top', bbox=props)
 
         fig.canvas.set_window_title(metric)
 
@@ -110,11 +85,6 @@ def visualize(result):
         plt.legend()
         plt.grid()
 
-    plt.show()
-    quit()
-        #
 
-    # metric
-    #     file
-    #         algorithm
+    plt.show()
 
